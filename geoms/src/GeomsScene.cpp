1d96780a07cd3188b680ceef5b32bcc8c9c74e76
mancsc <mancsc@hush.com>
2013-10-30 02:21:31 +0000

    Initial GeomsScene implementation

--- geoms/src/GeomsScene.cpp
+++ geoms/src/GeomsScene.cpp
@@ -7,3 +7,28 @@
 //
 
 #include "GeomsScene.h"
+
+
+#pragma mark - Class methods
+
+GeomsScene *GeomsScene::create(const char* id)
+{
+    GeomsScene *instance = (GeomsScene *)Scene::create(id);
+    instance->onLoad();
+    return instance;
+}
+
+GeomsScene* GeomsScene::load(const char* filePath)
+{
+    GeomsScene *instance = (GeomsScene *)Scene::load(filePath);
+    instance->onLoad();
+    return instance;
+}
+
+
+#pragma mark - Instance methods
+
+void GeomsScene::onLoad()
+{
+    
+}
